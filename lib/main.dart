95229948380f89cd0e06bee3f9809808aa81d445
Vincent Leone <vincent.leone@studenti.unipd.it>
2025-04-02 09:46:44 +0200

    01

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class RunBalanceApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'RunBalance',
+      title: 'RunBalance01',
       theme: ThemeData(primarySwatch: Colors.orange),
       home: SplashScreen(),
     );
